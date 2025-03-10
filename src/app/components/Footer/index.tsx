2922fe74b0207789f6a7ae63c4b4859c25b01c70
NikitaCherepov <100406501+NikitaCherepov@users.noreply.github.com>
2025-03-11 01:40:03 +0700

    [ADD] - Added basic responsive for sites

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -10,10 +10,16 @@ export default function Header() {
 
             <div className={styles.links}>
                 <p className={styles.links__header}>Контакты</p>
-                <div className={styles.links__buttons}>
+                <div className={`${styles.links__buttons} ${styles.links__buttons_desktop}`}>
                     <Button link='mailto:mkxvk@yandex.ru' size='small' icon={'/images/icons/email.svg'} text={'E-mail'}/>
                     <Button link={'https://t.me/hoursen'} size='small' icon={'/images/icons/tg.svg'} text={'чат'}/>
                     <Button link='https://vk.com/nikita_cherepov' size='small' icon={'/images/icons/vk.svg'} text={'лс'}/>
+                </div>
+
+                <div className={`${styles.links__buttons} ${styles.links__buttons_mobile}`}>
+                    <Button link='mailto:mkxvk@yandex.ru' size='small' icon={'/images/icons/email.svg'}/>
+                    <Button link={'https://t.me/hoursen'} size='small' icon={'/images/icons/tg.svg'}/>
+                    <Button link='https://vk.com/nikita_cherepov' size='small' icon={'/images/icons/vk.svg'}/>
                 </div>
             </div>
 
@@ -23,10 +29,16 @@ export default function Header() {
 
             <div className={styles.links}>
                 <p className={styles.links__header}>Соцсети</p>
-                <div className={styles.links__buttons}>
+                <div className={`${styles.links__buttons} ${styles.links__buttons_desktop}`}>
                     <Button link='https://www.youtube.com/@nikitacherepov' size='small' icon={'/images/icons/yt.svg'} text={'канал'}/>
                     <Button link='https://t.me/+7JUAL4jfnTUzMjBi' size='small' icon={'/images/icons/tg.svg'} text={'канал'}/>
                     <Button link='https://vk.com/hitchhikersimagination' size='small' icon={'/images/icons/vk.svg'} text={'группа'}/>
+                </div>
+
+                <div className={`${styles.links__buttons} ${styles.links__buttons_mobile}`}>
+                    <Button link='https://www.youtube.com/@nikitacherepov' size='small' icon={'/images/icons/yt.svg'}/>
+                    <Button link='https://t.me/+7JUAL4jfnTUzMjBi' size='small' icon={'/images/icons/tg.svg'}/>
+                    <Button link='https://vk.com/hitchhikersimagination' size='small' icon={'/images/icons/vk.svg'}/>
                 </div>
             </div>
 
