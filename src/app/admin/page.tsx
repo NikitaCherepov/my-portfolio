d7f279bcf0537beff91a5065b50218bb98dd9f65
NikitaCherepov <100406501+NikitaCherepov@users.noreply.github.com>
2025-11-10 02:07:01 +0700

    [CHANGE] - Changed styles for admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,19 +1,16 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import styles from './admin.module.scss';
 
 export default function AdminPage() {
   const router = useRouter();
 
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      fontSize: '18px'
-    }}>
-      Перенаправление на страницу входа...
+    <div className={`mainContainer ${styles.container}`}>
+      <div className={styles.container__redirect}>
+        Перенаправление на страницу входа...
+      </div>
     </div>
   );
 }
